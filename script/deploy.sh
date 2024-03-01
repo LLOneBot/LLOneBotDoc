ecf11849c245a59b472a989fb054d419b8cfc7d5
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-01 21:14:36 +0800

    chore:build

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,8 +2,16 @@ cd ..
 mkdir temp
 GITHUB_URL=https://oauth:${LLOneBot_GH_TOKEN}@github.com/LLOneBot/llonebot.github.io
 git clone ${GITHUB_URL} temp
+
+rm -rf temp/*
 mv ./LLOneBotDoc/docs/.vitepress/* temp
+
 cd ./temp
+
+git config --global init.defaultBranch main
+git remote add origin ${GITHUB_URL}
+git branch -M main
+
 git config --global user.name "Mlikiowa"
 git config --global user.email "nanaeonn@outlook.com"
 git add *
