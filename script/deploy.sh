99906aca7788bdd7f5f8849ae71f4a65d85f984a
手瓜一十雪 <nanaeonn@outlook.com>
2024-03-01 21:17:44 +0800

    fix: commit

--- script/deploy.sh
+++ script/deploy.sh
@@ -15,5 +15,5 @@ git branch -M main
 git config --global user.name "Mlikiowa"
 git config --global user.email "nanaeonn@outlook.com"
 git add *
-git commit -m "Automatic generation"
+git commit -m "docs: auto update"
 git push -f
